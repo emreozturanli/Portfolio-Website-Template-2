26e93f65bfec0f8c06679bc5168ee8b8e15b0e4a
Emre <98835168+emreozturanli@users.noreply.github.com>
2022-05-04 13:25:01 +0300

    version 5.0

--- script.js
+++ script.js
@@ -21,3 +21,45 @@ close.addEventListener('click', ()=>{
     hamburger.style.display = "block";
     close.style.display = "none";
 })
+
+
+/* BoundingClientRect */
+
+let elements = document.querySelectorAll('.grid-item');
+
+
+window.addEventListener('scroll', function(){
+
+    elements.forEach(element => {
+        if(element.getBoundingClientRect().top < window.innerHeight - 150){
+                element.classList.add('fade-right');
+            }
+    })
+    
+})
+
+let texts = document.querySelectorAll('.project-text');
+
+
+window.addEventListener('scroll', function(){
+
+    texts.forEach(text => {
+        if(text.getBoundingClientRect().top < window.innerHeight - 150){
+                text.classList.add('fade-top');
+            }
+    })
+    
+})
+
+let images = document.querySelectorAll('.project-image');
+
+
+window.addEventListener('scroll', function(){
+
+    images.forEach(image => {
+        if(image.getBoundingClientRect().top < window.innerHeight - 150){
+                image.classList.add('fade-bottom');
+            }
+    })
+    
+})
